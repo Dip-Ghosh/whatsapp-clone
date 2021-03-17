4239db74bb0d82f6200a6fabb209ecc929b2d57b
Dip Ghosh <dipnsucse@gmail.com>
2021-03-17 12:00:15 +0600

    Project Set up

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCKziK_qU76bMnwtF1_6LUpjwvo8MhKNFA",
+  authDomain: "whatsapp-clone-d4895.firebaseapp.com",
+  projectId: "whatsapp-clone-d4895",
+  storageBucket: "whatsapp-clone-d4895.appspot.com",
+  messagingSenderId: "540615597917",
+  appId: "1:540615597917:web:b29e505a23d8bf1fb012b4",
+  measurementId: "G-WCBGSBQQRT"
+};
